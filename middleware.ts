c04a52eba75936568862579c747323c8cd48138c
Aman Varshney <av.amanvarshney11@gmail.com>
2024-05-18 22:10:57 +0530

    Update dependencies and add user authentication middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/middleware";
 
 
 export async function middleware(request: NextRequest) {
   const { supabase, response } = await createClient(request);
-  await supabase.auth.getUser();
+  const { data: { user } } = await supabase.auth.getUser();
+
+  if (!user && !request.nextUrl.pathname.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/login", request.url))
+  }
+
   return response;
 }
+
 
 export const config = {
   matcher: [

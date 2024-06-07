7ed73bcadd38772bee27274b868a5bba42784bc3
Aman Varshney <av.amanvarshney11@gmail.com>
2024-06-08 01:36:52 +0530

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,7 @@ import { createClient } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
   const { supabase, response } = await createClient(request);
-  const { data: { user } } = await supabase.auth.getUser();
-
-  if (!user && !request.nextUrl.pathname.startsWith("/login")) {
-    return NextResponse.redirect(new URL("/login", request.url))
-  }
+  await supabase.auth.getUser();
 
   return response;
 }

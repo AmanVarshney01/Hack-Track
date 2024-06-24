540b16020b9b0c091a7e350524a290b04a5d4a30
Aman Varshney <av.amanvarshney11@gmail.com>
2024-06-24 17:53:52 +0530

    Add useEffect hook to ToastWrapper component

--- components/ToastWrapper.tsx
+++ components/ToastWrapper.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function ToastWrapper({
   message,
@@ -12,23 +13,19 @@ export default function ToastWrapper({
   goto?: string;
 }) {
   const router = useRouter();
-  if (label && goto) {
-    toast.info(message, {
-      action: {
-        label: label,
-        onClick: () => router.push(goto),
-      },
-    });
-  } else {
-    toast.info(message);
-  }
 
-  //   toast("Event has been created", {
-  //     description: "Sunday, December 03, 2023 at 9:00 AM",
-  //     action: {
-  //       label: "Undo",
-  //       onClick: () => console.log("Undo"),
-  //     },
-  //   })
+  useEffect(() => {
+    if (label && goto) {
+      toast.info(message, {
+        action: {
+          label: label,
+          onClick: () => router.push(goto),
+        },
+      });
+    } else {
+      toast.info(message);
+    }
+  }, []);
+
   return null;
 }
